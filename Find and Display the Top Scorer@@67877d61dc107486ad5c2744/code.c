cef0849fa9c94cfc0d6519cf3c8647d320344099
Muskan <muskanmittal249@gmail.com>
2025-04-11 11:38:26 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -11,11 +11,11 @@ int main(){
     for(int i=0;i<n;i++){
     scanf("%d %s %f",&s[i].RollNo,&s[i].Name,&s[i].Marks);
     }
-    int top_score=s[0];
+    int top_score=s[0].Marks;
     for(int i=0;i<n;i++){
         if(top_score.Marks<s[i].Marks){
             top_score=s[i].Marks;
         }
-     printf("Roll Number: %d, Name: %s,Marks: %.2f\n",top_score.RollNo,top_score.Name,top_score);}
+     printf("Roll Number: %d, Name: %s,Marks: %.2f\n",s[i].RollNo,s[i].Name,top_score);}
   return 0;
 }
